0a96e2260ed8e2fa18494328f9390fb15bd7fb05
jhiatt <jordan.hiatt11@gmail.com>
2020-04-02 23:01:42 -0400

    center cards

--- src/components/task.js
+++ src/components/task.js
@@ -6,13 +6,15 @@ class Task extends React.Component {
     render() {
         return (
             <div className="row">
-                <div className="card">
-                    <div className="card-content grey-text text-darken-4">
-                        <p>I am a very simple card. I am good at containing small bits of information.
-                        I am convenient because I require little markup to use effectively.</p>
-                    </div>
-                    <div className="card-action">
-                        <a className="waves-effect waves-light btn">button</a>
+                <div className="col s12 m6 offset-m3">
+                    <div className="card">
+                        <div className="card-content grey-text text-darken-4">
+                            <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                        </div>
+                        <div className="card-action">
+                            <a className="waves-effect waves-light btn">button</a>
+                        </div>
                     </div>
                 </div>
             </div>
