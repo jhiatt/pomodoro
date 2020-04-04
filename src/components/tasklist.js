4372fadbac442aec521ca77dacda42541213240e
jhiatt <jordan.hiatt11@gmail.com>
2020-04-04 19:45:46 -0400

    local task creation working

--- src/components/tasklist.js
+++ src/components/tasklist.js
@@ -4,7 +4,13 @@ import Task from './task';
 class TaskList extends React.Component {
     constructor(props) {
         super(props);
-        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            newDescription: '',
+            tasks: []
+
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
         this.tasklistTimerCall = this.tasklistTimerCall.bind(this);
     }
 
@@ -12,27 +18,50 @@ class TaskList extends React.Component {
         this.props.timerCall(time)
     }
 
-    handleClick(event) {
-        let time = Date.now()
-        console.log(Date(time))
+    handleChange(event) {
+        this.setState({newDescription: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        let tasks = this.state.tasks;
+        let newTask = {
+            description: this.state.newDescription,
+            status: "Ready"
+        }
+        this.setState({tasks: [...this.state.tasks, newTask ]});
+        this.setState({newDescription: ''});
     }
 
     render() {
+        const allTasks = this.state.tasks.map((item, index) =>
+            <Task description={item.description} status={item.status} key={index} timerCall2={this.tasklistTimerCall}/>
+        );
+
         return (
             <React.Fragment>
-                <Task timerCall2={this.tasklistTimerCall}/>
+                {allTasks}
                 <div className="row">
-                    <div className="col s12 m6 offset-m3">
-
-                        <div className="card deep-orange accent-3">
-                            <div className="card-content white-text">
-                                <span className="card-title">Add New</span>
-                                <span className="card-title">I am a very simple card. I am good at containing small bits of information.
-                                I am convenient because I require little markup to use effectively.</span>
-                            </div>
-                            <div className="card-action">
-                                <a className="waves-effect waves-light btn" onClick={this.handleClick}>Add</a>
-                            </div>
+                    <div className="col s12 m6">
+                        <div id="new-task" className="card deep-orange accent-3">
+                            <form onSubmit={this.handleSubmit}  >
+                                <div className="card-content white-text">
+                                    <span className="card-title">Add New Task</span>
+                                    <div class="input-field">
+                                        <input 
+                                            placeholder="description" 
+                                            id="description" 
+                                            type="text" 
+                                            class="validate"  
+                                            onChange={this.handleChange}
+                                            value={this.state.newDescription}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="card-action">
+                                    <input className="waves-effect waves-light btn" type="submit" value="Add" />
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
